308acf40141ec04ef1aad0278012887be7464aa4
hyhwa414 <hyhwa414@gmail.com>
2021-08-08 16:28:04 +0900

    2021.08.07 first app with some buttons and image view

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 정현화 on 2021/08/08.
+//  Copyright © 2021 com.hyhwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
