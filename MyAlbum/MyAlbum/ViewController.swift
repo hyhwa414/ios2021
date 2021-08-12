565979a33ae45ea3e89afb9e5506961fc4b5539f
hyhwa414 <hyhwa414@gmail.com>
2021-08-12 12:15:15 +0900

    2021.08.07 basic of swift and app developing

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        refresh()
     }
 
+    
+    @IBAction func showAlert(_ sender: Any) {
+        let message = "가격은 ₩\(currentValue) 입니다"
+        
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in
+            self.refresh()
+        })
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func refresh() {
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        priceLabel.text = "W \(currentValue)"
+    }
+    
 }
 
---
+++ Playground/MyPlayground01.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//var str = "Hello, playground"
+//
+//let value = arc4random_uniform(100)
+//print("--> \(value)")
+
+
+// --- Tuple
+
+let coordinates = (4, 6)
+
+let x = coordinates.0
+let y = coordinates.1
+
+let coordinatesNamed = (x: 2, y: 3)
+
+let x2 = coordinatesNamed.x
+let y2 = coordinatesNamed.y
+
+let (x3, y3) = coordinatesNamed
+
+x3
+y3
